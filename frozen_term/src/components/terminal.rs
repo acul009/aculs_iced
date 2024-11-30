db4a9698b961e93dac262b97873a0b70810443c1
L. Wlcek <luca@it-rahn.de>
2024-11-30 01:39:57 +0100

    the actual problem does seem to be the rendering performance

--- frozen_term/src/components/terminal.rs
+++ frozen_term/src/components/terminal.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
 use iced::{
+    advanced::Widget,
     keyboard::key,
-    widget::{container, rich_text},
+    widget::{container, rich_text, text},
     Border, Element, Length, Shadow,
 };
 use wezterm_term::{
@@ -54,46 +55,58 @@ impl Terminal {
         <Theme as iced::widget::container::Catalog>::Class<'a>:
             From<iced::widget::container::StyleFn<'a, Theme>>,
     {
+        let mut basic = String::new();
         let screen = self.term.screen();
-        let palette = self.term.palette();
-
         let lines =
             screen.lines_in_phys_range(screen.phys_range(&(0..screen.physical_rows as i64)));
 
-        let width = screen.physical_cols;
-
-        let mut spans =
-            Vec::with_capacity(self.term.screen().physical_cols * self.term.screen().physical_rows);
-
-        // screen.for_each_phys_line(|index, line| {
         for line in lines {
-            let mut lines_found = 0;
-
-            for cell in line.visible_cells() {
-                lines_found += 1;
-                let style = cell.attrs();
-
-                let span = iced::widget::span(cell.str().to_string())
-                    .color(get_color(style.foreground(), &palette, true))
-                    .background(get_color(style.background(), &palette, false));
-                spans.push(span);
-            }
-
-            spans.push(iced::widget::span(" ".repeat(width - lines_found)));
-            spans.push(iced::widget::span("\n"));
+            basic.push_str(&line.as_str());
+            basic.push('\n');
         }
-        // });
 
-        let (r, g, b, a) = palette.background.to_tuple_rgba();
+        return text(basic).font(iced::Font::MONOSPACE);
 
-        container(rich_text(spans).font(iced::Font::MONOSPACE)).style(move |theme| {
-            container::Style {
-                text_color: None,
-                background: Some(iced::Color::from_rgba(r, g, b, a).into()),
-                border: Border::default().color(iced::Color::from_rgb(1.0, 1.0, 1.0)),
-                shadow: Shadow::default(),
-            }
-        })
+        // let screen = self.term.screen();
+        // let palette = self.term.palette();
+
+        // let lines =
+        //     screen.lines_in_phys_range(screen.phys_range(&(0..screen.physical_rows as i64)));
+
+        // let width = screen.physical_cols;
+
+        // let mut spans =
+        //     Vec::with_capacity(self.term.screen().physical_cols * self.term.screen().physical_rows);
+
+        // // screen.for_each_phys_line(|index, line| {
+        // for line in lines {
+        //     let mut lines_found = 0;
+
+        //     for cell in line.visible_cells() {
+        //         lines_found += 1;
+        //         let style = cell.attrs();
+
+        //         let span = iced::widget::span(cell.str().to_string())
+        //             .color(get_color(style.foreground(), &palette, true))
+        //             .background(get_color(style.background(), &palette, false));
+        //         spans.push(span);
+        //     }
+
+        //     spans.push(iced::widget::span(" ".repeat(width - lines_found)));
+        //     spans.push(iced::widget::span("\n"));
+        // }
+        // // });
+
+        // let (r, g, b, a) = palette.background.to_tuple_rgba();
+
+        // container(rich_text(spans).font(iced::Font::MONOSPACE)).style(move |theme| {
+        //     container::Style {
+        //         text_color: None,
+        //         background: Some(iced::Color::from_rgba(r, g, b, a).into()),
+        //         border: Border::default().color(iced::Color::from_rgb(1.0, 1.0, 1.0)),
+        //         shadow: Shadow::default(),
+        //     }
+        // })
     }
 
     pub fn key_press(&mut self, key: iced::keyboard::Key, modifiers: iced::keyboard::Modifiers) {
@@ -175,5 +188,45 @@ fn get_color(color: ColorAttribute, palette: &ColorPalette, foreground: bool) ->
             let (r, g, b, a) = color.to_tuple_rgba();
             iced::Color::from_rgba(r, g, b, a)
         }
+    }
+}
+
+pub struct TerminalWidget<'a> {
+    terminal: &'a Terminal,
+}
+
+impl<'a> TerminalWidget<'a> {
+    pub fn new(terminal: &'a Terminal) -> TerminalWidget<'a> {
+        TerminalWidget { terminal }
+    }
+}
+
+impl<'a, Message, Theme, Renderer: iced::advanced::Renderer> Widget<Message, Theme, Renderer>
+    for TerminalWidget<'a>
+{
+    fn size(&self) -> iced::Size<Length> {
+        todo!()
+    }
+
+    fn layout(
+        &self,
+        tree: &mut iced::advanced::widget::Tree,
+        renderer: &Renderer,
+        limits: &iced::advanced::layout::Limits,
+    ) -> iced::advanced::layout::Node {
+        todo!()
+    }
+
+    fn draw(
+        &self,
+        tree: &iced::advanced::widget::Tree,
+        renderer: &mut Renderer,
+        theme: &Theme,
+        style: &iced::advanced::renderer::Style,
+        layout: iced::advanced::Layout<'_>,
+        cursor: iced::advanced::mouse::Cursor,
+        viewport: &iced::Rectangle,
+    ) {
+        todo!()
     }
 }
